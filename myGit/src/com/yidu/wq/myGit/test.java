604c556f67f43a4368a9e31a21c0bd9e2c578669
Mila0519 <1305102929@qq.com>
2019-11-28 14:23:36 +0800

    delete file

--- myGit/src/com/yidu/wq/myGit/test.java
+++ myGit/src/com/yidu/wq/myGit/test.java
@@ -3,10 +3,6 @@ package com.yidu.wq.myGit;
 public class test {
 	public static void main(String[] args) {
 
-		System.out.println("����");
-		
-		System.out.println("ÍõÈºÊÇ¸öº©º©");
-		System.out.println("git to local");
 
 	}
 }
