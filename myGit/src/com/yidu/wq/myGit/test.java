b16fd862891a671de132d2fd21ae1b2e7fa08717
Mila0519 <57904430+Mila0519@users.noreply.github.com>
2019-11-28 14:17:32 +0800

    git to local
    git to local

--- myGit/src/com/yidu/wq/myGit/test.java
+++ myGit/src/com/yidu/wq/myGit/test.java
@@ -2,7 +2,7 @@ package com.yidu.wq.myGit;
 
 public class test {
 	public static void main(String[] args) {
-		System.out.println("ÍõÈºÊÇ¸öº©º©");
-		
+		System.out.println("ÃÃµÃˆÂºÃŠÃ‡Â¸Ã¶ÂºÂ©ÂºÂ©");
+		System.out.println("git to local");
 	}
 }
